2ace7c33c94e8f59bb9c8c2b546f6edfcb1dafef
Timothy Adamcik <tadamcik0425@gmail.com>
2023-11-22 09:43:09 -0500

    add tvos target

---
+++ Flix-/Flix- tvOS/Flix__tvOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flix__tvOSApp.swift
+//  Flix- tvOS
+//
+//  Created by Timothy Adamcik on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Flix__tvOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

e6b352b1eac5befd4801ac4301217bf916bbccd1
Timothy Adamcik <tadamcik0425@gmail.com>
2023-11-22 10:39:18 -0500

    add home view

---
+++ Flix-/Flix- iOS/Views/HomeView/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Flix-
+//
+//  Created by Timothy Adamcik on 11/22/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Welcome to Flix-")
+    }
+}
+
+#Preview {
+    HomeView()
+}

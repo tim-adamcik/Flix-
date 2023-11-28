1585bfc314114d5805aff58200cf1c35c02e1f0d
Timothy Adamcik <tadamcik0425@gmail.com>
2023-11-28 15:18:10 -0500

    Updated sample movies to include: year, rating, seasons, and promotional text

--- Flix-/Shared/GlobalHelpers.swift
+++ Flix-/Shared/GlobalHelpers.swift
@@ -6,16 +6,15 @@
 //
 
 import Foundation
-
-let exampleMovie1 = Movie(name: "Dark", thumbnailURL: URL(string: "https://picsum.photos/200/300")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi", "Thriller"])
-let exampleMovie2 = Movie(name: "Community", thumbnailURL: URL(string: "https://picsum.photos/200/301")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi", "Thriller"])
-let exampleMovie3 = Movie(name: "South Park", thumbnailURL: URL(string: "https://picsum.photos/200/302")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi", "Thriller"])
-let exampleMovie4 = Movie(name: "Annabel", thumbnailURL: URL(string: "https://picsum.photos/200/303")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi", "Thriller"])
-let exampleMovie5 = Movie(name: "After Life", thumbnailURL: URL(string: "https://picsum.photos/200/304")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi", "Thriller"])
-let exampleMovie6 = Movie(name: "Hannibal", thumbnailURL: URL(string: "https://picsum.photos/200/305")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi", "Thriller"])
-let exampleMovie7 = Movie(name: "Shark Tale", thumbnailURL: URL(string: "https://picsum.photos/200/306")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi", "Thriller"])
-let exampleMovie8 = Movie(name: "Jumanji", thumbnailURL: URL(string: "https://picsum.photos/200/307")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi", "Thriller"])
-let exampleMovie9 = Movie(name: "Pets", thumbnailURL: URL(string: "https://picsum.photos/200/308")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi", "Thriller"])
+let exampleMovie1 = Movie(name: "Dark", thumbnailURL: URL(string: "https://picsum.photos/200/300")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi", "Thriller"], year: 2020, rating: "TV-MA")
+let exampleMovie2 = Movie(name: "Community", thumbnailURL: URL(string: "https://picsum.photos/200/301")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi", "Thriller"], year: 2020, rating: "TV-MA", numberOfSeasons: 1, promotionalHeadline: "Best New Show")
+let exampleMovie3 = Movie(name: "South Park", thumbnailURL: URL(string: "https://picsum.photos/200/302")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi", "Thriller"], year: 2020, rating: "TV-MA", numberOfSeasons: 2)
+let exampleMovie4 = Movie(name: "Annabel", thumbnailURL: URL(string: "https://picsum.photos/200/303")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi", "Thriller"], year: 2020, rating: "TV-MA", numberOfSeasons: 3)
+let exampleMovie5 = Movie(name: "After Life", thumbnailURL: URL(string: "https://picsum.photos/200/304")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi", "Thriller"], year: 2020, rating: "TV-MA", numberOfSeasons: 4, promotionalHeadline: "New Episodes Coming Soon")
+let exampleMovie6 = Movie(name: "Hannibal", thumbnailURL: URL(string: "https://picsum.photos/200/305")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi", "Thriller"], year: 2020, rating: "TV-MA", numberOfSeasons: 5)
+let exampleMovie7 = Movie(name: "Shark Tale", thumbnailURL: URL(string: "https://picsum.photos/200/306")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi", "Thriller"], year: 2020, rating: "TV-MA", numberOfSeasons: 6, promotionalHeadline: "Watch Season 6 now")
+let exampleMovie8 = Movie(name: "Jumanji", thumbnailURL: URL(string: "https://picsum.photos/200/307")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi", "Thriller"], year: 2020, rating: "TV-MA", numberOfSeasons: 7)
+let exampleMovie9 = Movie(name: "Pets", thumbnailURL: URL(string: "https://picsum.photos/200/308")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi", "Thriller"], year: 2020, rating: "TV-MA", numberOfSeasons: 1)
 
 let exampleMovies: [Movie] = [
     exampleMovie1,

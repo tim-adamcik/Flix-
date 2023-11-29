4e8c7cda620cf51288ae2b6a9058053f930502da
Timothy Adamcik <tadamcik0425@gmail.com>
2023-11-29 17:43:37 -0500

    Episode view and finish off changes to movie detail view

--- Flix-/Flix- iOS/Views/MovieDetailView/Episodes/EpisodesView.swift
+++ Flix-/Flix- iOS/Views/MovieDetailView/Episodes/EpisodesView.swift
@@ -8,11 +8,69 @@
 import SwiftUI
 
 struct EpisodesView: View {
+    
+    var episodes: [Episode]
+    @Binding var showSeasonPicker: Bool
+    @Binding var selectedSeason: Int
+    
+    func getEpisodes(forSeason season: Int) -> [Episode] {
+        return episodes.filter({ $0.season == season })
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 14) {
+            // Season Picker
+            HStack {
+                Button {
+                    showSeasonPicker = true
+                } label: {
+                    Group {
+                        Text("Season \(selectedSeason)")
+                        Image(systemName: "chevron.down")
+                    }
+                    .font(.system(size: 16))
+                    .foregroundStyle(.gray)
+                }
+                Spacer()
+            }
+            
+            
+            
+            // Episodes List
+            ForEach(getEpisodes(forSeason: selectedSeason)) { episode in
+                VStack(alignment: .leading) {
+                    HStack(spacing: 20) {
+                        VideoPreviewImage(imageURL: episode.thumbnailUrl!, videoURL: episode.videoURL)
+                            .frame(width: 120, height: 70)
+                            .clipped()
+                        
+                        VStack(alignment: .leading) {
+                            Text("\(String(episode.episodeNumber)). \(episode.name)")
+                                .foregroundStyle(.white)
+                            Text(episode.formattedEpisodeDuration)
+                                .font(.caption)
+                                .foregroundStyle(.gray)
+                        }
+                        Spacer()
+                        Image(systemName: "arrow.down.to.line.alt")
+                            .foregroundStyle(.white)
+                    }
+                    .padding(10)
+                    Text(episode.description)
+                        .font(.system(size: 13))
+                        .lineLimit(3)
+                        .foregroundStyle(.white)
+                }
+                .padding(.bottom, 20)
+                
+            }
+        }
     }
 }
 
 #Preview {
-    EpisodesView()
+    ZStack {
+        Color.black
+        EpisodesView(episodes: exampleEpisodes, showSeasonPicker: .constant(false), selectedSeason: .constant(2))
+    }
 }
--- Flix-/Flix- iOS/Views/MovieDetailView/MovieDetailView.swift
+++ Flix-/Flix- iOS/Views/MovieDetailView/MovieDetailView.swift
@@ -11,6 +11,8 @@ struct MovieDetailView: View {
     
     var movie: Movie
     @State var seasonSelection: Int = 1
+    @State private var showSeasonPicker = false
+    @State private var selectedSeason = 1
     
     private var screen: CGRect {
             guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
@@ -24,71 +26,95 @@ struct MovieDetailView: View {
             Color.black
                 .ignoresSafeArea()
             
-            VStack {
-                HStack {
-                    Spacer()
-                    Button {
-                        // close the view
-                    } label: {
-                        Image(systemName: "xmark.circle")
-                            .font(.system(size: 28))
+            ZStack {
+                VStack {
+                    HStack {
+                        Spacer()
+                        Button {
+                            // close the view
+                        } label: {
+                            Image(systemName: "xmark.circle")
+                                .font(.system(size: 28))
+                        }
+                    }
+                    .padding(.horizontal, 22)
+                    
+                    ScrollView(.vertical, showsIndicators: false) {
+                        VStack {
+                            StandardHomeMovie(movie: movie)
+                                .frame(width: screen.width / 2.5)
+                            
+                            MovieInfoSubviewView(movie: movie)
+                            if movie.promotionalHeadline != nil {
+                                Text(movie.promotionalHeadline!)
+                                    .font(.headline)
+                                    .bold()
+                            }
+                            SmallHorizontalButton(text: "Play", image: "play.fill", backgroundColor: .red, foregroundColor: .white) {
+                                // play movie
+                            }
+                            CurrentEpisodeInformationView(movie: movie)
+                            CastInfoView(movie: movie)
+                            
+                            HStack(spacing: 60) {
+                                SmallVerticalButton(text: "My List", onImage: "checkmark", offImage: "plus", isOn: true) {
+                                    //
+                                }
+                                SmallVerticalButton(text: "Rate", onImage: "hand.thumbsup.fill", offImage: "hand.thumbsdown.fill", isOn: true) {
+                                    //
+                                }
+                                SmallVerticalButton(text: "Share", onImage: "square.and.arrow.up", offImage: "square.and.arrow.up", isOn: true) {
+                                    //
+                                }
+                                Spacer()
+                                
+                            }
+                            .padding(.leading, 20)
+
+                            CustomTabSwitcher(tabs: [.episodes,.trailers,.more], movie: movie, selectedSeason: $selectedSeason, showSeasonPicker: $showSeasonPicker)
+
+                        }
+                        .padding(.horizontal, 8)
                     }
                 }
-                .padding(.horizontal, 22)
+                .foregroundStyle(.white)
                 
-                ScrollView(.vertical, showsIndicators: false) {
-                    VStack {
-                        StandardHomeMovie(movie: movie)
-                            .frame(width: screen.width / 2.5)
-                        
-                        MovieInfoSubviewView(movie: movie)
-                        if movie.promotionalHeadline != nil {
-                            Text(movie.promotionalHeadline!)
-                                .font(.headline)
-                                .bold()
-                        }
-                        SmallHorizontalButton(text: "Play", image: "play.fill", backgroundColor: .red, foregroundColor: .white) {
-                            // play movie
-                        }
-                        CurrentEpisodeInformationView(movie: movie)
-                        CastInfoView(movie: movie)
-                        
-                        HStack(spacing: 60) {
-                            SmallVerticalButton(text: "My List", onImage: "checkmark", offImage: "plus", isOn: true) {
-                                //
-                            }
-                            SmallVerticalButton(text: "Rate", onImage: "hand.thumbsup.fill", offImage: "hand.thumbsdown.fill", isOn: true) {
-                                //
-                            }
-                            SmallVerticalButton(text: "Share", onImage: "square.and.arrow.up", offImage: "square.and.arrow.up", isOn: true) {
-                                //
-                            }
+                if showSeasonPicker {
+                    Group {
+                        Color.black.opacity(0.9)
+                            .ignoresSafeArea()
+                        VStack {
                             Spacer()
                             
+                            ForEach(0..<(movie.numberOfSeasons ?? 0)) { season in
+                                Button {
+                                    selectedSeason = season + 1
+                                    showSeasonPicker = false
+                                } label: {
+                                    Text("Season \(season + 1)")
+                                }
+                                .foregroundStyle(season + 1 == selectedSeason ? .white : .gray)
+                                .font(season + 1 == selectedSeason ? .title : .title2)
+                                .bold()
+                                
+                            }
+                            .padding(4)
+                            
+                            Spacer()
+                            
+                            Button {
+                                showSeasonPicker = false
+                            } label: {
+                                Image(systemName: "x.circle.fill")
+                                    .foregroundStyle(.white)
+                                    .font(.system(size: 40))
+                                    .scaleEffect(x: 1.1)
+                            }
+                            .padding(.bottom, 30)
                         }
-                        .padding(.leading, 20)
-
-                        CustomTabSwitcher(tabs: [.episodes,.trailers,.more], movie: movie)
-
-                        
-                    }
-                    .padding(.horizontal, 8)
-                }
-                
-                Spacer()
-                Spacer()
-                
-                if movie.numberOfSeasons != nil {
-                    Picker(selection: $seasonSelection) {
-                        ForEach(0..<movie.numberOfSeasons!, id: \.self) { season in
-                            Text("Season \(String(season))")
-                        }
-                    } label: {
-                        Text("Seasons")
                     }
                 }
             }
-            .foregroundStyle(.white)
         }
     }
 }
